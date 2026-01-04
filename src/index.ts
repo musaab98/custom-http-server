9621da0a5514dc8d7a8d12e42fbc512f63f298a7
musaab98 <elsheikh.musaab@gmail.com>
2026-01-03 23:58:39 -0500

    asdjke

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
-app.listen(8080)
+const PORT = 8080;
+
+// Serve static files under /app (files located in ./src/app)
+app.use("/app", express.static("./src/app"));
+
+// Readiness endpoint
+function handlerReadiness(_req: Request, res: Response) {
+  res.set("Content-Type", "text/plain; charset=utf-8");
+  res.send("OK");
+}
+
+app.all("/healthz", handlerReadiness);
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
